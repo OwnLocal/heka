38317edee4887501b3048e36424e6bca8cf65051
Paul Bonser <misterpib@gmail.com>
2016-05-26 23:22:51 -0500

    Point to OL fork of mozilla services plugins

--- cmake/externals.cmake
+++ cmake/externals.cmake
@@ -167,7 +167,7 @@ if (INCLUDE_DOCKER_PLUGINS)
 endif()
 
 if (INCLUDE_MOZSVC)
-    add_external_plugin(git https://github.com/mozilla-services/heka-mozsvc-plugins 91278658b5d52bd45b0b74d54e478a230c0ef0c4)
+    add_external_plugin(git https://github.com/OwnLocal/heka-mozsvc-plugins 39d8dcabf472e95df7bb84be74c95eb8903cd05f)
     git_clone(https://github.com/getsentry/raven-go 0cc1491d9d27b258a9b4f0238908cb0d51bd6c9b)
     add_dependencies(heka-mozsvc-plugins raven-go)
 endif()
